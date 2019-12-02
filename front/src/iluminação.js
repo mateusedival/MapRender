7d0c72e4cc02f4b03c69ac710ae83fd058790b3a
Joao.Gavron <joao.gavron@gmail.com>
2019-12-02 16:51:51 -0300

    Add files via upload

--- front/src/iluminação.js
+++ front/src/iluminação.js
@@ -1,58 +1,62 @@
+///FUNCAO DE SOMBREAMENTO CONSTANTE PRINCIPAL
 function SomConstante(Ka, Kd, Ks, Il, Ila, n, face, LVetor, N) {
     let Centro = new Ponto();
-    Centro = CalculaCentroGeo(face);
+    let a = face.centroide;
+    let b = face.normal;
 
     let Ia = Ila * Ka;
-    let Id = CalculaDifusa(LVetor, N, Centro);
-    let Is = CalculaEspecular(Lvetor, N, VRP, Centro);
+    let Id = CalculaDifusa(LVetor, N, a, Il, Kd);
+    let Is = CalculaEspecular(Lvetor, N, VRP, a, Il, Ks, n);
 
     let It = Ia + Id + Is;
     return It;
 }
 
-function CalculaCentroGeo(face) {
-    let CentroMax = new Ponto(Math.max(face.aresta.ponto.x), Math.max(face.aresta.ponto.y), Math.max(face.aresta.ponto.z)); //nao sei
-    let CentroMin = new Ponto(Math.min(face.aresta.ponto.x), Math.min(face.aresta.ponto.y), Math.min(face.aresta.ponto.z));
-    let Centro = new Ponto(((CentroMax.x + CentroMin.x) / 2), ((CentroMax.y + CentroMin.y) / 2), ((CentroMax.z + CentroMin.z) / 2));
+function CalculaL(V, Ponto) {
+    let L = new Vetor((V.i - Ponto.x, V.j - Ponto.y, V.k - Ponto.z));
+    L = L.Normaliza();
 
-    return Centro;
+    return L;
 }
 
-function CalculaL(V, Centro) {
-    let L = new Vetor((V.x - Centro.x, V.y - Centro.y, V.z - Centro.z));
-
-    Vetor = Vetor.Normaliza();
-
-    return Vetor;
-}
-
-function CalculaEspecular(LVetor, N, VRP, Centro) {
+function CalculaEspecular(LVetor, face, VRP, Centro, Il, Ks, n) {
     let R = new Vetor();
     let taxa;
-    taxa = 2 * (ProdutoEscalar(N, LVetor));
-    R = N.Subtracao(LVetor);
-    R = ((taxa * R.x), (taxa * R.y), (taxa * R.z));
+    let a = face.normal;
+
+    taxa = 2 * (ProdutoEscalar(a, LVetor));
+    R = ((taxa * a.i), (taxa * a.j), (taxa * a.k));
+    R = R.Subtracao(LVetor);
 
     let S = new Vetor();
-    S = VPR.Subtracao(Centro);
+    S = VRP.Subtracao(Centro);
     S = S.Normaliza();
 
-    let Is = ProdutoEscalar(R, S);
-    return Is;
+    let RS = ProdutoEscalar(R, S);
+
+    if (RS > 0) {
+        RS = Math.pow(RS, n);
+        return (Il * Ks * RS);
+    } else
+        return 0;
+
+
 }
 
-function CalculaDifusa(LVetor, N, Centro) {
+function CalculaDifusa(LVetor, N, a, Il, Kd) {
     let L = new Vetor();
+    L = CalculaL(LVetor, a);
+    L = L.Normaliza();
+    L = L.ProdutoEscalar(N);
 
-    L = CalculaL(LVetor, Centro);
-    let lzinho = L.ProdutoEscalar(N);
-    let Id = Il - Kd(lzinho);
-
-    return Id;
+    if (L > 0)
+        return (Il * Kd * L);
+    else
+        return 0;
 }
 
 
-ProdutoEscalar(outro) {
+function ProdutoEscalar(outro) {
     let produto;
 
     produto = (this.i * outro.i) + (this.j * outro.j) + (this.k * outro.k);
