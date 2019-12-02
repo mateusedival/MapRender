48f6bec3fc5234c7e8fd95bd077e1fe1736e64db
Joao.Gavron <joao.gavron@gmail.com>
2019-12-02 09:11:32 -0300

    VRP, merge e outras cousitas

--- front/src/classes.js
+++ front/src/classes.js
@@ -1,11 +1,14 @@
-export class Ponto {
-    constructor(x, y, z) {
+class Ponto
+{
+    constructor(x, y, z)
+    {
         this.x = x || 0;
         this.y = y || 0;
         this.z = z || 0;
     }
 
-    CalculoVetor(outro) {
+    CalculoVetor(outro)
+    {
         let vetor = new Vetor();
 
         vetor.i = this.x - outro.x;
@@ -15,28 +18,34 @@ export class Ponto {
         return vetor;
     }
 
-    Desloca(Tx, Ty, Tz) {
+    Desloca(Tx, Ty, Tz)
+    {
         this.x += Tx;
         this.y += Ty;
         this.z += Tz;
     }
 
-    Imprime() {
+    Imprime()
+    {
         console.log("x: " + this.x + "\ny: " + this.y + "\nz: " + this.z);
     };
 }
 
-class Aresta {
-    constructor(p, q) {
+class Aresta
+{
+    constructor(p, q)
+    {
         this.p = p || new Point();
         this.q = q || new Point();
     }
 
-    get vetor() {
+    get vetor()
+    {
         return Jorge.CalculoVetorEntre2Pontos(this.p, this.q);
     }
 
-    Imprime() {
+    Imprime()
+    {
         console.log("Ponto p:");
         this.p.Imprime();
 
@@ -47,20 +56,22 @@ class Aresta {
     }
 }
 
-
-class Face {
-    constructor(a, b, c) {
+class Face
+{
+    constructor(a, b, c)
+    {
         this.a = a || new Aresta();
         this.b = b || new Aresta();
         this.c = c || new Aresta();
     }
 
-    // TODO: expressar a regra da mao direita para obter a normal da face
-    get normal() {
+    get normal()
+    {
         return 0;
     }
 
-    Imprime() {
+    Imprime()
+    {
         console.log("Aresta A-B");
         this.a.Imprime();
 
@@ -72,28 +83,34 @@ class Face {
     }
 }
 
-export class SuperFace {
-    constructor() {
+export class SuperFace
+{
+    constructor()
+    {
         this.faces = new Map();
     }
 
-    AddConjuntoFaces(nomeConjunto, conjunto) {
+    AddConjuntoFaces(nomeConjunto, conjunto)
+    {
         this.faces.set(nomeConjunto, conjunto);
     }
 
-    Imprime(nomeConjunto) {
+    Imprime(nomeConjunto)
+    {
         let arrayFaces;
 
-        if (arrayFaces = this.faces.get(nomeConjunto)) {
+        if (arrayFaces = this.faces.get(nomeConjunto))
+        {
             arrayFaces.forEach((f) => f.Imprime());
         }
     }
 }
 
 
-
-class Vetor {
-    constructor(i, j, k) {
+class Vetor
+{
+    constructor(i, j, k)
+    {
         this.i = i || 0;
         this.j = j || 0;
         this.k = k || 0;
@@ -105,20 +122,19 @@ class Vetor {
         this.modulo;
     }
 
-    Normaliza() {
-        let vetor = new Vetor();
-
+    Normaliza()
+    {
         this.modulo = Math.pow(this.i, 2) + Math.pow(this.j, 2) + Math.pow(this.k, 2);
         this.modulo = Math.sqrt(this.modulo);
 
-        vetor.i = this.i / this.modulo;
-        vetor.j = this.j / this.modulo;
-        vetor.k = this.k / this.modulo;
-
-        return vetor;
+        console.log (this.modulo);
+        this.norm_i = this.i / this.modulo;
+        this.norm_j = this.j / this.modulo;
+        this.norm_k = this.k / this.modulo;
     }
 
-    ProdutoVetorial(outro) {
+    ProdutoVetorial(outro)
+    {
         let produto = new Vetor();
 
         produto.i = (this.j * outro.z) - (this.z * outro.y);
@@ -128,91 +144,105 @@ class Vetor {
         return produto;
     }
 
+    ProdutoEscalar(outro) 
+    {
+        if (outro instanceof Vetor)
+            return ((this.i * outro.i) + (this.j * outro.j) + (this.k * outro.k));
+        else
+            return ((this.i * outro.x) + (this.j * outro.y) + (this.k * outro.z));
 
-    ProdutoEscalar(outro) {
-        let produto;
-
-        produto = (this.i * outro.i) + (this.j * outro.j) + (this.k * outro.k);
-
-        return produto;
     }
 
-    Subtracao(outro) {
-        return new Vetor(this.i - outro.i,
-            this.j - outro.j,
-            this.k - outro.k);
+    Subtracao (outro)
+    {
+        if (outro instanceof Ponto)
+            return new Vetor (this.i - outro.x, this.j - outro.y, this.k - outro.z);
+        else
+            return new Vetor (this.i - outro.i, this.j - outro.j, this.k - outro.k);
     }
 
-    Imprimi() {
-        console.log("VETOR NAO-NORMALIZADO:");
-        console.log("i: " + this.i + "\nj: " + this.j + "\nk: " + this.k);
-    }
-
-    ImprimiNormalizado() {
-        console.log("VETOR NORMALIZADO:");
-        console.log("i: " + this.norm_i + "\nj: " + this.norm_j + "\nk: " + this.norm_k);
+    // normalizado é uma booleana para qual versao do vetor vc quer ver
+    // true = vetor NORMALIZADO
+    // false = vetor NAO NORMALIZADO
+    Imprimi(normalizado)
+    {
+        if (!normalizado)
+            console.log("i: " + this.i + ", j: " + this.j + ", k: " + this.k);
+        else
+            console.log("i: " + this.norm_i + "\nj: " + this.norm_j + "\nk: " + this.norm_k);
     }
 }
 
-// TODO: TERMINAR O VRP
-class VRP extends Vetor {
-    constructor(x, y, z, _pontoFocal) {
+class VRP extends Vetor
+{
+    constructor(x, y, z, _pontoFocal)
+    {
         super(x, y, z);
 
-        this.pontoFocal = _pontoFocal || new Vetor(0, 0, -10);
+        this.pontoFocal = _pontoFocal || new Vetor (0, 0, -10);
 
+        this.CalculaNVU();
+        
         this.n;
         this.v;
         this.u;
     }
 
-    GetN() {
-        if (!this.n) {
-            this.n = this.Subtracao(this.pontoFocal);
-            this.n.Normaliza();
-        }
+    CalculaN()
+    {
+        if (!this.n)
+            this.n = new Vetor();
+        
+        this.n = this.Subtracao(this.pontoFocal);
+        this.n.Normaliza();
 
         return this.n;
     }
 
-    GetV() {
-        if (!this.v) {
-            v.x = this.n.norm_j * this.n.norm_i;
-            v.y = this.n.norm_j * this.n.norm_j;
-            v.z = this.n.norm_j * this.n.norm_k;
+    CalculaV()
+    {
+        if (!this.v)
+            this.v = new Vetor();
+        
+        this.v.i = this.n.norm_j * this.n.norm_i;
+        this.v.j = this.n.norm_j * this.n.norm_j;
+        this.v.k = this.n.norm_j * this.n.norm_k;
 
-            v.x = 0 - v.x;
-            v.y = 1 - v.y;
-            v.z = 0 - v.z;
-        }
+        this.v.i = 0 - this.v.i;
+        this.v.j = 1 - this.v.j;
+        this.v.k = 0 - this.v.k;
+
+        this.v.Normaliza();
 
         return this.v;
     }
 
-    // get v (Nnormalizado)
-    // {
-    //     let v = new Ponto();
+    CalculaU ()
+    {
+        if (!this.u)
+            this.u = new Vetor ();
+            
+        this.u.norm_i = (this.v.norm_j * this.n.norm_k) - (this.v.norm_k * this.n.norm_j);
+        this.u.norm_j = (this.v.norm_k * this.n.norm_i) - (this.v.norm_i * this.n.norm_k);
+        this.u.norm_k = (this.v.norm_i * this.n.norm_j) - (this.v.norm_j * this.n.norm_i);
 
-    //     v.x = Nnormalizado.y * Nnormalizado.x;
-    //     v.y = Nnormalizado.y * Nnormalizado.y;
-    //     v.z = Nnormalizado.y * Nnormalizado.z;
+        return this.u;
+    }
 
-    //     v.x = 0 - v.x;
-    //     v.y = 1 - v.y;
-    //     v.z = 0 - v.z;
-    //     return CalculaNormal(v);
+    CalculaNVU ()
+    {
+        this.CalculaN();
+        this.CalculaV();
+        this.CalculaU();
+    }
 
+    ImprimiNVU ()
+    {
+        console.log ("N: {" + this.n.norm_i + ", " + this.n.norm_j + ", " + this.n.norm_k + "}");
+        console.log ("V: {" + this.v.norm_i + ", " + this.v.norm_j + ", " + this.v.norm_k + "}");
+        console.log ("U: {" + this.u.norm_i + ", " + this.u.norm_j + ", " + this.u.norm_k + "}");
+    }
 }
-// get u (N, V)
-// {
-//     let produto = new Ponto();
-//     produto.x = (V.y * N.z) - (V.z * N.y);
-//     produto.y = (V.z * N.x) - (V.x * N.z);
-//     produto.z = (V.x * N.y) - (V.y * N.x);
-
-//     return CalculaNormal(produto);
-// }
-//}
 
 //DANIZITA COMEÇOU A ALTERAR ISSO
 class Jorge {
